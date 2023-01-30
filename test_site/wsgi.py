90513e89c0f8d88dfffc4d4d495085210265929f
tadashiorigami <tadashimori.br@gmail.com>
2023-01-30 20:43:30 -0300

    Update wsgi.py

--- test_site/wsgi.py
+++ test_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_site.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_site.settings.production')
 
 application = get_wsgi_application()
